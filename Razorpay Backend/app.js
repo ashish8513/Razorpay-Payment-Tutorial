0ea4e6cfe75f4f945cde8c2e291615220f0b5c96
Ashish Prabhakar <141730191+ashish8513@users.noreply.github.com>
2024-09-18 15:20:14 +0530

    Update app.js

--- Razorpay Backend/app.js
+++ Razorpay Backend/app.js
@@ -2,26 +2,15 @@ import express from "express";
 import { config } from "dotenv";
 import paymentRoute from "./routes/paymentRoutes.js";
 import cors from "cors";
-import morgan from "morgan";
 config({ path: "./config/config.env" });
 
 export const app = express();
-
-app.use(cors(
-  {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    credentials: true,
-  }
-
-));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'))//for the https  request for the server
 
-//here the code for the calling the router in the middleware 
 app.use("/api", paymentRoute);
 
 app.get("/api/getkey", (req, res) =>
   res.status(200).json({ key: process.env.RAZORPAY_API_KEY })
 );
+// Bro, don't copy-paste Try to do it yourself Best of luck
